b83c76fbe0bc588905102f028e93f664c3fbaf54
Jon Taylor <jopataylor@wccnet.edu>
2021-10-04 21:54:48 -0400

    Working Calculator

--- TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tipcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tipcalculator.databinding.ActivityMainBinding
+import android.view.View
+import android.view.animation.AccelerateInterpolator
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        binding.theButton.setOnClickListener {
+
+            if (binding.billAmount.text.toString() == "") {
+                binding.calculatedTips.text = "You Must Enter A Bill Amount"
+            } else {
+
+                var validBill = Integer.parseInt(binding.billAmount.text.toString())
+
+                binding.calculatedTips.text =
+                    "10% = " + String.format("%.2f", validBill * 1.10) + "\n" +
+                            "15% = " + String.format("%.2f", validBill * 1.15) + "\n" +
+                            "20% = " + String.format("%.2f", validBill * 1.20) + "\n"
+
+
+            }
+
+
+        }
+
+
     }
 }
