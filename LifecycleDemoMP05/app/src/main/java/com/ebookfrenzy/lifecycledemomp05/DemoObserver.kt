bc970614e1f5f830c877fd989e80ef49c498ffd0
Jon Taylor <jopataylor@wccnet.edu>
2021-10-27 13:07:14 -0400

    Module 8 Update

---
+++ LifecycleDemoMP05/app/src/main/java/com/ebookfrenzy/lifecycledemomp05/DemoObserver.kt
@@ -0,0 +1,42 @@
+package com.ebookfrenzy.lifecycledemomp05
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+
+
+class DemoObserver : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+
+    }
+
+
+}
---
+++ LifecycleDemoMP05/app/src/main/java/com/ebookfrenzy/lifecycledemomp05/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.ebookfrenzy.lifecycledemomp05.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+    
+}
